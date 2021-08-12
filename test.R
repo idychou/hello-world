5dcdb3cd664ce936263d73ee7e6742c4fcbb7a96
idychou <88828057+idychou@users.noreply.github.com>
2021-08-12 18:22:50 +0800

    Update test.R

--- test.R
+++ test.R
@@ -1,3 +1,5 @@
-test <- function(){
-  cat('Hello World!')
-  }
+# test <- function(){
+#   code=sprintf("mutt -e 'set content_type=text/html' -s %s %s -c %s -a %s < %s", '香港中文大學疫症情緒健康研究：網上評估［個人報告］', subj_info$email, 'wmhsurvey@cuhk.edu.hk', encryptedPdfFile, emailFile)
+# 	system(code)
+#   }
+cat('Hello World!')
